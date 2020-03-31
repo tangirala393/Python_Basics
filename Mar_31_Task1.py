268961e83987dcd06676e87e2e465236c9268d43
krishna reddy <62807128+tangirala393@users.noreply.github.com>
2020-03-31 21:24:32 +0530

    Add files via upload

---
+++ Mar_31_Task1.py
@@ -0,0 +1,52 @@
+a= int(input("enter value of a:"))
+b= int(input("enter value of b:"))
+
+'''
+
+#Program for a^2 - b^2
+c=(a*a) -(b*b)
+
+print(int(c))
+
+
+
+#Program for  (a+b) * (a-b)
+
+c=(a+b)*(a-b)
+print(int(c))
+
+
+
+
+
+#Program for  (a+b)^2
+
+c=(a+b)*(a+b)
+print(int(c))
+
+
+
+
+
+#Program for  (a+b)^2
+
+c=(a*a)+(b*b)+(2*a*b)
+print(int(c))
+
+
+
+
+
+#Program for Volume of Cylinder
+
+r=int(input("enter the radius of Cylinder:"))
+pi=3.14
+h=int(input("height of cylinder:"))
+
+volume=pi* (r*r) *h
+print("volume of Cylinder:",int(volume))
+
+
+'''
+
+
