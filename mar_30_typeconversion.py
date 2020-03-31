02c9f0da8af5e8b88dc6d6a135f038d47ca1cc0a
krishna reddy <62807128+tangirala393@users.noreply.github.com>
2020-03-31 21:23:33 +0530

    Add files via upload

---
+++ mar_30_typeconversion.py
@@ -0,0 +1,139 @@
+a = 9
+
+k = 16.6
+
+x = "pyhton"
+
+z = True
+
+'''
+#Type conversion: int to  int
+print(a)
+print(type(a))
+b = int(a)
+print(b)
+print(type(b))
+
+#Type conversion: int to  float
+print(a)
+print(type(a))
+b = float(a)
+print(b)
+print(type(b))
+
+
+#Type conversion: int to str
+print(a)
+print(type(a))
+b = str(a)
+print(b)
+print(type(b))
+
+#Type conversion: int to bool
+print(a)
+print(type(a))
+b = bool(a)
+print(b)
+print(type(b))
+
+
+
+#Type conversion: float to int
+print(k)
+print(type(k))
+b = int(k)
+print(b)
+print(type(b))
+
+
+
+#Type conversion: float to float
+print(k)
+print(type(k))
+b = float(k)
+print(b)
+print(type(b))
+
+
+#Type conversion: float to str
+print(k)
+print(type(k))
+b = str(k)
+print(b)
+print(type(b))
+
+#Type conversion: float to bool
+print(k)
+print(type(k))
+b = bool(k)
+print(b)
+print(type(b))
+
+#Type conversion: str to int
+print(x)
+print(type(x))
+b = int(x)
+print(b)
+print(type(b))
+
+
+#Type conversion: str to float
+print(x)
+print(type(x))
+b = float(x)
+print(b)
+print(type(b))
+
+
+#Type conversion: str to str
+print(x)
+print(type(x))
+b = str(x)
+print(b)
+print(type(b))
+
+
+
+#Type conversion: str to bool
+print(x)
+print(type(x))
+b = bool(x)
+print(b)
+print(type(b))
+
+
+
+#Type conversion: Bool to int
+print(z)
+print(type(z))
+b = int(z)
+print(b)
+print(type(b))
+
+
+#Type conversion: Bool to float
+print(z)
+print(type(z))
+b = float(z)
+print(b)
+print(type(b))
+
+
+
+#Type conversion: Bool to str
+print(z)
+print(type(z))
+b = str(z)
+print(b)
+print(type(b))
+
+
+#Type conversion: Bool to bool
+print(z)
+print(type(z))
+b = bool(z)
+print(b)
+print(type(b))
+
+'''
+
