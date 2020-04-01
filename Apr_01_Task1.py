04cd626dabba142622f4d10613eebd40a947ffd2
krishna reddy <62807128+tangirala393@users.noreply.github.com>
2020-04-01 19:50:30 +0530

    Add files via upload

---
+++ Apr_01_Task1.py
@@ -0,0 +1,20 @@
+
+'''
+r=float(input("enter radius of circle:"))
+pi=3.14
+
+area =int( pi * (r**2))
+
+print("Area of circle with r{0}  is {1} ".format(r,area))
+
+
+
+'''
+
+
+r=float(input("enter radius of circle:"))
+pi=3.14
+
+perimeter =int( 2 * pi * r )
+
+print("Perimeter of Circle with r{0} is {1}".format(r,perimeter))
