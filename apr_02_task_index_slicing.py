1ef17f0a952f43bc9c4698f902503e5cbf545665
krishna reddy <62807128+tangirala393@users.noreply.github.com>
2020-04-02 09:30:23 +0530

    Add files via upload

---
+++ apr_02_task_index_slicing.py
@@ -0,0 +1,53 @@
+
+
+#Task1
+
+v = "computers"
+print(v[-1]) #v[7]
+print(v[3]) #v[-5]
+print(v[0]) #v[-8]
+
+print(len(v))
+print(v[int(len(v)/2)])  #to print middle char of string
+
+
+
+
+#Task2
+
+s="engineering_mathematics"
+print(len(s))
+print(s[-1])
+print(s[-2])
+print(s[0])
+print(s[1])
+
+print(  s[int(len(s)/2)])  ##to print middle char of string
+
+print(s[-11:])
+print(s[2:10])
+print(s[15:])
+print(s[3:12])
+print(s[-19:-9])
+print(s[2:-9])
+print(s[-18:18])
+print(s[-18:])
+print(s[0:6])
+
+
+
+#Task3
+
+s="engineering_mathematics"
+
+print(s[12:25])     #to print  mathematics
+print(s[:11])       #to print engineering
+print(s[17:25])     #to print matics
+print(s[4:8])       #to print neer
+print(s[12:15])     #to print mat
+
+
+
+
+
+
