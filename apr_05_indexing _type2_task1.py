2597ca68f35afe4428289bc11f8addc9476f5800
krishna reddy <62807128+tangirala393@users.noreply.github.com>
2020-04-05 21:44:35 +0530

    Add files via upload

---
+++ apr_05_indexing _type2_task1.py
@@ -0,0 +1,24 @@
+a="computer_science"
+'''
+print(a[5:-5:5])    # tc
+print(a[5:-5:-5])#empty
+print(a[-5:5:-5])#ie
+print(a[-5:5:5])#empty
+print(a[4:-4:4])#u_
+print(a[4:-4:-4])#empty
+print(a[-4:4:-4])#e_
+print(a[:-4:-4])#empty
+'''
+
+
+print(a[::-3])#eesepc
+print(a[::-5])#ectc
+print(a[::3])#cpesee
+print(a[::4])#cu_e
+print(a[::-8])#er
+print(a[::-5])#ectc
+print(a[::-4])#eirp
+print(a[::8])#c_
+print(a[::1])#computer_science
+print(a[::-1])#ecneics_retupmoc
+print(a[::2])#cmue_cec
