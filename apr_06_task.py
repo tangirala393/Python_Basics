a0fc60d76369db390823824f31eceb8f87f4714b
krishna reddy <62807128+tangirala393@users.noreply.github.com>
2020-04-07 07:08:00 +0530

    Add files via upload

---
+++ apr_06_task.py
@@ -0,0 +1,84 @@
+
+'''
+
+#To print PythoN
+a = input("enter string:")
+a = a[0].upper()+a[1:5]+a[-1].upper()
+print(a)
+
+
+#To print pYthOn
+a = a[0]+a[1].upper()+a[2:4]+a[-2].upper()+a[-1]
+print(a)
+
+
+#To print pythonpythonpythonpythonpythonpythonpython777777
+
+
+a = input("enter the String:")
+b = input("enter the number :")
+print(a*7)
+print(b)
+
+print(  (a * 7) + ( b * len(a))  )
+
+
+
+#To print firefirefirefirefire5555
+
+a = input("enter the string:")
+b = input("enter the num:")
+
+print( (a * 5)  + (b * len(a))  )
+
+
+#To print i/o: python o/p: PytHoN
+
+a = input("enter the string:")
+print(a[0].upper() + a[1:3] +a[3].upper() +a[-2] +a[-1].upper() )
+
+
+
+#To print i/p: python, java  o/p: PythonNJavA
+
+a = input("enter the string1:")
+b = input("enter the string2:")
+
+c = a[0].upper()+a[1:6]+a[-1].upper()
+d = b[0].upper()+b[1:3]+b[-1].upper()
+
+print(c+d)
+
+
+#To print  55533333
+
+x = input("enter the first num:")
+y = input("enter the second num:")
+
+print( (x*3) +(y*5))
+
+
+
+'''
+
+print(a)
+
+b=a[0].upper()
+c=a[-1].upper()
+print(b)
+print(type(b))
+print(c)
+print(type(c))
+
+print(a)
+print(type(a))
+d = a[1:5]
+
+print(d)
+print(type(d))
+
+a = b+d+c
+print(a)
+'''
+
+
